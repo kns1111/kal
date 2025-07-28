45349348450a3219cac8ec0414e3ae9150b7cda9
kns1111 <kalyaninikhilsalvi@gmail.com>
2025-07-28 23:37:24 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,8 +1,6 @@
 
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+a=10
+b=20
+sum=a+b
+print("sum:",sum)
 
-
-sum = float(num1) + float(num2)
-
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
